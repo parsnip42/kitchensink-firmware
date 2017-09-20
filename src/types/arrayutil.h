7c0beac7a2a06f8e942b6ab690b6e12523783c17
parsnip42 <parsniphead42@gmail.com>
2017-09-20 14:53:59 +0100

    Simplify arrayutil

--- src/types/arrayutil.h
+++ src/types/arrayutil.h
@@ -8,53 +8,43 @@ template<typename Array>
 class ArrayUtil
 {
 private:
-    // Returns an std::array with each element populated from func. This is
-    // achieved by expanding the Indices parameter pack, so it'll usually be
-    // called as such;
-    //    mapArray<Func, 0, 1, 2>(func)
-    //
-    // which will then create an array equivalent to;
-    //    std::array<T, Size>{{ func(0), func(1), func(2) }}
-    template<typename Func, std::size_t... Indices>
-    static constexpr Array mapArray(const Func& func)
-    { 
-        return Array{{ func(Indices)... }};
-    }
-
     // This class uses variadic templates recursively to create a parameter pack
-    // of indices from 0 to Size - 1.
+    // of indices from 0 to Size - 1, and then creates an array from the
+    // expanded parameter pack.
     template<std::size_t... Indices>
-    class Sequence;
+    class CreateArray;
 
     // Terminates the sequence by specialising for 0 as the first element
-    // (because the parameter pack is created in reverse).
+    // (because the parameter pack is created in reverse), then returns an
+    // std::array with each element populated from func, ie;
+    //     Array{{ func(0), func(1), func(2) }}
     template<std::size_t... Indices>
-    class Sequence<0, Indices...>
+    class CreateArray<0, Indices...>
     {
     public:
         template <typename Func>
-        static constexpr Array createArray(const Func& func)
+        static constexpr Array create(const Func& func)
         {
-            return mapArray<Func, 0, Indices...>(func);
+            return Array{{ func(0), func(Indices)... }};
         }
     };
 
     // Recursively creates a new sequence class by adding indices in descending
     // order, ie;
-    //     Sequence<2, 3>::createArray(func)
+    //     CreateArray<2, 3>::createArray(func)
     //
     // calls;
-    //     Sequence<1, 2, 3>::createArray(func)
+    //     CreateArray<1, 2, 3>::createArray(func)
     //
     // Until we reach the specialisation above.
     template<std::size_t First, std::size_t... Indices>
-    class Sequence<First, Indices...>
+    class CreateArray<First, Indices...>
     {
     public:
         template <typename Func>
-        static constexpr Array createArray(const Func& func)
+        static constexpr Array create(const Func& func)
         {
-            return Sequence<First - 1, First, Indices...>::createArray(func);
+            return CreateArray<First - 1, First, Indices...>::create(func);
         }
     };
 
@@ -62,7 +52,7 @@ public:
     template <typename Func>
     static constexpr Array createArray(const Func& func)
     {
-        return Sequence<std::tuple_size<Array>::value - 1>::createArray(func);
+        return CreateArray<std::tuple_size<Array>::value - 1>::create(func);
     }
 };
 
